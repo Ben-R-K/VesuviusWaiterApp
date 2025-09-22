2d8438504c70e47c52017e1e7d9416399732fc29
Ben Krüger <eaglebenraphael@web.de>
2025-09-22 08:14:43 +0200

    Final update

---
+++ waiterapp/lib/Views/reservations_overview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ReservationsOverviewPage extends StatefulWidget {
+  const ReservationsOverviewPage({super.key});
+
+  @override
+  State<ReservationsOverviewPage> createState() =>
+      RreservationsOverviewPageState();
+}
+
+class RreservationsOverviewPageState extends State<ReservationsOverviewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
