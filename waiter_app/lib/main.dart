d310ecd5d13c6b4410100fe200ee9b5b9599540d
Skole computer <rolfvejsig@gmail.com>
2025-09-30 08:44:07 +0200

    tjener yaay

--- waiter_app/lib/main.dart
+++ waiter_app/lib/main.dart
@@ -2,14 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:waiter_app/screens/table_overview.dart';
 import 'package:waiter_app/services/session_manager.dart';
 import 'package:waiter_app/theme/app_theme.dart';
+import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Replace 'DEV_TOKEN' with a real token for connecting to your authenticated backend.
+  HttpOverrides.global = MyHttpOverrides();
   final sessionManager = SessionManager(autoLoginToken: 'DEV_TOKEN');
   await sessionManager.detectAndSetWorkingBaseUrl();
   runApp(MyApp(sessionManager: sessionManager));
 }
+
+
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   final SessionManager sessionManager;
