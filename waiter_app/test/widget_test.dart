855e9feaf2d83d47b91376f8b7e72fcd6ba7aacb
rolfVejsig <rolfvejsig@gmail.com>
2025-10-07 09:57:27 +0200

    forbedret widget test

--- waiter_app/test/widget_test.dart
+++ waiter_app/test/widget_test.dart
@@ -5,19 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:waiter_app/main.dart';
 import 'package:waiter_app/services/session_manager.dart';
+import 'package:waiter_app/screens/table_overview.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('TableOverviewScreen vises korrekt', (WidgetTester tester) async {
     final sessionManager = SessionManager();
     await tester.pumpWidget(MyApp(sessionManager: sessionManager));
 
-  // Verify that the menu screen is shown
-  expect(find.text('Menu'), findsOneWidget);
+    // Tjek at TableOverviewScreen vises
+    expect(find.byType(TableOverviewScreen), findsOneWidget);
   });
-} 
+}
